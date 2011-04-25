bf65b1ef8042087168ad389e9d5fd335947709bd
Jannis Leidel <jannis@leidel.info>
2011-04-25 16:38:38 +0200

    No reason to try importlib, really.

--- formwizard/storage/__init__.py
+++ formwizard/storage/__init__.py
@@ -1,9 +1,6 @@
 from django.core.exceptions import ImproperlyConfigured
+from django.utils.importlib import import_module
 
-try:
-    from importlib import import_module
-except ImportError:
-    from django.utils.importlib import import_module
 
 class MissingStorageModuleException(ImproperlyConfigured):
     pass
